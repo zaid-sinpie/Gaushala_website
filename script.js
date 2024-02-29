b0637b85766373c6a84ee611a35f4e603895b1e8
zaid-sinpie <zaidmasuldar@gmail.com>
2024-03-01 00:01:08 +0530

    responsive folder

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const home = document.querySelector('.home-tag');
+const homeDropdown = document.querySelector('.home-tag-dropdown');
 const homeRedirect = document.querySelector('.section-1');
 
+
+//home redirect buttons
 home.addEventListener('click',function(e){
+    e.preventDefault();
+    homeRedirect.scrollIntoView({
+        behavior:'smooth'
+    });
+});
+homeDropdown.addEventListener('click',function(e){
     e.preventDefault();
     homeRedirect.scrollIntoView({
         behavior:'smooth'
