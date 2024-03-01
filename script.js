7b35cda66f7510449c85711973c09915302d789f
zaid-sinpie <zaidmasuldar@gmail.com>
2024-03-01 15:35:50 +0530

    images slideshhow implemented

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ homeDropdown.addEventListener('click',function(e){
         behavior:'smooth'
     });
 });
+
+//images animation slideshow
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
