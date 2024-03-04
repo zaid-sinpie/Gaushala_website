89ecf42ddf1ba5927c172f92a3ce2bc4183053ea
zaid-sinpie <zaidmasuldar@gmail.com>
2024-03-04 20:06:12 +0530

    about us footer added

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const home = document.querySelector(".home-tag");
 const homeDropdown = document.querySelector(".home-tag-dropdown");
 const community = document.querySelector(".community-tag");
 const communityDropdown = document.querySelector(".community-tag-dropdown");
+const about = document.querySelector('.about-tag');
+const aboutDropdown = document.querySelector('.about-tag-dropdown');
 
 const donate = document.querySelector(".donate");
 
 //sections
 const homeRedirect = document.querySelector(".section-1");
 const communityRedirect = document.querySelector(".section-2");
+const aboutRedirect = document.querySelector('.about');
 
 //home redirect buttons
 home.addEventListener("click", function (e) {
@@ -39,6 +42,21 @@ communityDropdown.addEventListener("click", function (e) {
     behavior: "smooth",
   });
 });
+
+//about redirect button
+about.addEventListener("click", function (e) {
+  e.preventDefault();
+  aboutRedirect.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+aboutDropdown.addEventListener("click", function (e) {
+  e.preventDefault();
+  aboutRedirect.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
 
 //images animation slideshow
 let myIndex = 0;
